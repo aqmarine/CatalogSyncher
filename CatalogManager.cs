c3cd806e86b6c4bdfd3cc1e01fab4c0fefedfa65
Marina Osipkova <aqmarine@bk.ru>
2021-10-04 15:32:22 +0200

    Creating, deleting directories; creating, deleting and moving files.

---
+++ CatalogManager.cs
@@ -0,0 +1,156 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CatalogSyncher
+{
+    public class CatalogManager
+    {
+        //?
+        //path - подразумевается root
+        //relative path уже есть в файле
+        public static void AddFiles(IEnumerable<MyFile> files, string path)
+        {
+            ProcessFile(files, path, CatalogItemAction.Add, File.Copy);
+        }    
+
+        private static void ProcessFile(IEnumerable<MyFile> files, string rootPath, CatalogItemAction action, Action<string, string> actionFunc)
+        {
+            var processableFiles = files.Where(i => i.CatalogItemAction == action);
+            try
+            {
+                foreach (var item in processableFiles)
+                {
+                    var sourceFileName = item.Info.FullName;
+                    var destFileName = Path.Combine(rootPath, item.RelativePath);
+                    actionFunc(sourceFileName, destFileName);
+                }
+            }
+            catch(Exception e)
+            {
+                System.Console.WriteLine("couldn't add files");
+            }
+        }
+
+        public static void MoveFiles(IEnumerable<MyFile> files, string path)
+        {
+            ProcessFile(files, path, CatalogItemAction.Rename, File.Move);
+        }
+
+        public static void DeleteFiles(IEnumerable<MyFile> files)
+        {
+            var removableFiles = files.Where(i => i.CatalogItemAction == CatalogItemAction.Delete);
+            try
+            {
+                foreach (var item in removableFiles)
+                {
+                    item.Info.Delete();
+                }
+            }
+            catch(Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+        }
+
+        internal static void ManageDirectories(MyDirectory dir)
+        {
+            ICollection<MyDirectory> dirs = null;
+            FindAddableOrRemovableDirectories(dir, ref dirs);
+            ProcessFolders(dirs);
+        }
+
+        private static void ProcessFolders(ICollection<MyDirectory> dirs)
+        {
+            try
+            {
+                foreach (var item in dirs)
+                {
+                    switch(item.CatalogItemAction)
+                    {
+                        case CatalogItemAction.Add:
+                            Directory.CreateDirectory(item.Info.FullName);
+                            break;
+                        case CatalogItemAction.Delete:
+                            Directory.Delete(item.Info.FullName);
+                            break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.Message);
+            }
+        }
+
+     
+
+        public static void AddDirectories(MyDirectory sourceDir, string rootPath)
+        {
+            try
+            {
+                if(sourceDir.CatalogItemAction == CatalogItemAction.Add)
+                {
+                    var path = Path.Combine(rootPath, sourceDir.RelativePath);
+                    Directory.CreateDirectory(path);
+                }
+                var subDirs = sourceDir.Subdirectories;
+                if (subDirs != null)
+                {
+                    foreach (var item in subDirs)
+                    {
+                        AddDirectories(item, rootPath);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.Message);
+            }
+        }
+
+        public static void DeleteDirectories(MyDirectory sourceDir)
+        {
+            try
+            {
+                if(sourceDir.CatalogItemAction == CatalogItemAction.Delete)
+                {
+                    sourceDir.Info.Delete(true);
+                }
+                else
+                {
+                    var subDirs = sourceDir.Subdirectories;
+                    if (subDirs != null)
+                    {
+                        foreach (var item in subDirs)
+                        {
+                            DeleteDirectories(item);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.Message);
+            }
+        } 
+
+        private static void FindAddableOrRemovableDirectories(MyDirectory dir, ref ICollection<MyDirectory> foundDirs)
+        {
+            var subDirs = dir.Subdirectories;
+            if(subDirs != null)
+            {
+                if(dir.CatalogItemAction == CatalogItemAction.Add
+                            || dir.CatalogItemAction == CatalogItemAction.Delete)
+                {
+                    foundDirs.Add(dir);
+                }
+                foreach (var item in subDirs)
+                {
+                    FindAddableOrRemovableDirectories(item, ref foundDirs);
+                }
+            }
+        }
+    }
+}
