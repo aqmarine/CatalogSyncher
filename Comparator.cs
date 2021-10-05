1dd0e00a01723dd5b0c21912fdf0322e7a34d11c
Marina Osipkova <aqmarine@bk.ru>
2021-10-05 16:26:22 +0200

    Refactoring

--- Comparator.cs
+++ Comparator.cs
@@ -9,48 +9,23 @@ namespace CatalogSyncher
     public class Comparator
     {
         private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
-        
-        public void CompareTwoFilesDictionaries(IDictionary<byte[], MyFile> source, IDictionary<byte[], MyFile> replic)
-        {
-            //CleanDictionaries(source, replic);
-            //check not checked files
-            // var sourceFiles = source.Where(i => i.Value.WasChecked = false).Select(i => i.Key, i.Value);
-            // var replicFiles = replic.Where(i => i.Value.WasChecked = false);
-            VerifyСorrectness(source, replic);
-        }
 
-        private void CleanDictionaries(IDictionary<byte[], MyFile> source, IDictionary<byte[], MyFile> replic)
-        {
-            //это листы
-            var addableFiles = source.Except(replic).Select(i => i.Value).ToList();
-            var removableFiles = replic.Except(source).Select(i => i.Value).ToList();
-            MarkAsAddable(addableFiles);
-            MarkAsRemovable(removableFiles);
-        }
-
-        private void VerifyСorrectness(IDictionary<byte[], MyFile> source, IDictionary<byte[], MyFile> replic)
+        public static void CompareTwoFileDictionaries(IDictionary<byte[], MyFile> source, IDictionary<byte[], MyFile> replic)
         {
             foreach (var item in source)
             {
                 if (!replic.TryGetValue(item.Key, out MyFile currentReplicItem))
                 {
                     item.Value.WasChecked = true;
-                    item.Value.CatalogItemAction = CatalogItemAction.Add;
+                    item.Value.CatalogItemAction = CatalogItemAction.Create;
                     continue;
                 }
                 //если файл перенесли или переименовали, то поменять положение
                 if (item.Value.RelativePath != currentReplicItem.RelativePath)
                 {
                     currentReplicItem.RelativePath = item.Value.RelativePath;
-                    //currentReplicItem.CatalogItemAction = CatalogItemAction.Replace;
                     currentReplicItem.CatalogItemAction = CatalogItemAction.Rename;
                 }
-                //if(item.Value.Info.Name != currentReplicItem.Info.Name)
-                // if(item.Value.RelativePath != currentReplicItem.RelativePath)
-                // {
-                //     //не здесь же переименовывать???
-                //     currentReplicItem.CatalogItemAction = CatalogItemAction.Rename;
-                // }
                 item.Value.WasChecked = true;
                 currentReplicItem.WasChecked = true;
             }
@@ -61,97 +36,19 @@ namespace CatalogSyncher
                 item.WasChecked = true;
             }
         }
-        public static void Compare(MyDirectory a, ref MyDirectory b)
-        {
-            if(a.Info.Name != b.Info.Name)
-            {
-                b.CatalogItemAction = CatalogItemAction.Rename;
-            }
-            var filesA = a.Files;
-            var filesB = b.Files;
-            CompareSetOfFiles(filesA, filesB);
 
-            var dirs = a.Subdirectories;
-            //?то же самое будет работать с папками
-            
-            
-        }
-
-        private static void CompareSetOfFiles(ICollection<MyFile> filesA, ICollection<MyFile> filesB)
-        {
-            if(filesA == null && filesB == null)
-            {
-
-            }
-            if(filesA == null && filesB != null)
-            {
-                MarkAsRemovable(filesB);
-            }
-            if(filesA != null && filesB == null)
-            {
-                MarkAsAddable(filesA);
-            }
-            if(filesA != null && filesB != null)
-            {
-                CheckFiles(filesA, filesB);
-            }
-        }
-        private static void CheckFiles(ICollection<MyFile> filesA, ICollection<MyFile> filesB)
-        {
-            var dicSource = new Dictionary<byte[], MyFile>();
-            foreach (var item in filesA)
-            {
-                dicSource.Add(item.GetHashMD5(), item);
-            }
-            foreach (var item in filesB)
-            {
-                var itemHash = item.GetHashMD5();
-                if(dicSource.ContainsKey(itemHash))
-                {
-                    if (dicSource[itemHash].Info.Name != item.Info.Name)
-                    {
-                        item.CatalogItemAction = CatalogItemAction.Rename;
-                    }
-                    dicSource[itemHash].WasChecked = true;
-                    item.WasChecked = true;
-                    dicSource.Remove(itemHash);
-                }
-            }
-            MarkAsRemovable(filesB.Where(b => b.WasChecked == false));
-            MarkAsAddable(dicSource.Values);
-        }
-
-        private static void MarkAsRemovable(IEnumerable<MyFile> collection)
-        {
-            foreach (var item in collection)
-            {
-                item.WasChecked = true;
-                item.CatalogItemAction = CatalogItemAction.Delete;
-            }
-        }
-
-        private static void MarkAsAddable(IEnumerable<MyFile> collection)
-        {
-            foreach (var item in collection)
-            {
-                item.WasChecked = true;
-                item.CatalogItemAction = CatalogItemAction.Add;
-            }
-        }
-
-
-        public void CompareTwoDirectories(MyDirectory source, MyDirectory replic)
+        public static void CompareTwoDirectories(MyDirectory source, MyDirectory replic)
         {
             if(source.RelativePath == replic.RelativePath)
             {
                 var subSourceDirs = source.Subdirectories;
                 var subReplicDirs = replic.Subdirectories;
-                GoDeep(subSourceDirs, subReplicDirs);
+                CompareSubdirectories(subSourceDirs, subReplicDirs);
             }
             FindRemovableDirectories(replic);
         }
 
-        private void FindRemovableDirectories(MyDirectory replic)
+        private static void FindRemovableDirectories(MyDirectory replic)
         {
             var subDirs = replic.Subdirectories;
             if (subDirs != null)
@@ -167,7 +64,7 @@ namespace CatalogSyncher
             }
         }
 
-        private void GoDeep(ICollection<MyDirectory> source, ICollection<MyDirectory> replic)
+        private static void CompareSubdirectories(IReadOnlyCollection<MyDirectory> source, IReadOnlyCollection<MyDirectory> replic)
         {
             if(source != null)
             {
@@ -185,11 +82,11 @@ namespace CatalogSyncher
                     if(hsReplic.TryGetValue(item, out MyDirectory currentDir))
                     {
                         currentDir.WasChecked = true;
-                        GoDeep(item.Subdirectories, currentDir.Subdirectories);
+                        CompareSubdirectories(item.Subdirectories, currentDir.Subdirectories);
                     }
                     else
                     {
-                        item.CatalogItemAction = CatalogItemAction.Add;
+                        item.CatalogItemAction = CatalogItemAction.Create;
                         MarkAllSubdirectoriesAsAddable(item);
                     }
                     item.WasChecked = true;
@@ -198,14 +95,14 @@ namespace CatalogSyncher
             
         }
 
-        private void MarkAllSubdirectoriesAsAddable(MyDirectory root)
+        private static void MarkAllSubdirectoriesAsAddable(MyDirectory root)
         {
             var subDirs = root.Subdirectories;
             if(subDirs != null)
             {
                 foreach (var item in subDirs)
                 {
-                    item.CatalogItemAction = CatalogItemAction.Add;
+                    item.CatalogItemAction = CatalogItemAction.Create;
                     MarkAllSubdirectoriesAsAddable(item);
                 }
             }
