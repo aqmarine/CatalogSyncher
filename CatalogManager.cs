fcfa4c54559eea437d24e974e6bee1143e7659f2
Marina Osipkova <aqmarine@bk.ru>
2021-10-05 15:53:49 +0200

    refactoring

--- CatalogManager.cs
+++ CatalogManager.cs
@@ -10,151 +10,152 @@ namespace CatalogSyncher
     {
         private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
 
-        //?
-        //path - подразумевается root
-        //relative path уже есть в файле
-        public static void AddFiles(IEnumerable<MyFile> files, string path)
-        {
-            ProcessFile(files, path, CatalogItemAction.Add, File.Copy);
-        }    
 
-        private static void ProcessFile(IEnumerable<MyFile> files, string rootPath, CatalogItemAction action, Action<string, string> actionFunc)
+        public static void CreateFiles(IEnumerable<MyFile> files, string rootPath)
         {
-            var processableFiles = files.Where(i => i.CatalogItemAction == action);
-            try
-            {
-                foreach (var item in processableFiles)
-                {
-                    var sourceFileName = item.Info.FullName;
-                    var destFileName = Path.Combine(rootPath, item.RelativePath);
-                    actionFunc(sourceFileName, destFileName);
-                    _logger.Info("С файлом {file} была проведена операция {operation}", item.Info.Name, action);
-                }
-            }
-            catch(Exception e)
-            {
-                _logger.Error(e, "couldn't add files");
-            }
+            ProcessFile(files, rootPath, CatalogItemAction.Create, File.Copy);
         }
 
-        public static void MoveFiles(IEnumerable<MyFile> files, string path)
+        public static void MoveFiles(IEnumerable<MyFile> files, string rootPath)
         {
-            ProcessFile(files, path, CatalogItemAction.Rename, File.Move);
+            ProcessFile(files, rootPath, CatalogItemAction.Rename, File.Move);
         }
 
         public static void DeleteFiles(IEnumerable<MyFile> files)
         {
             var removableFiles = files.Where(i => i.CatalogItemAction == CatalogItemAction.Delete);
-            try
+            foreach (var item in removableFiles)
             {
-                foreach (var item in removableFiles)
+                try
                 {
                     item.Info.Delete();
+                    _logger.Info("The file {file} was deleted ", item.RelativePath);
+                }
+                catch(Exception e)
+                {
+                    _logger.Error(e, "Couldn't delete file {name}", item.RelativePath);
                 }
             }
-            catch(Exception e)
-            {
-                _logger.Error(e, "todo");
-            }
+            
         }
 
-        internal static void ManageDirectories(MyDirectory dir)
+        public static void CreateDirectories(MyDirectory sourceDir, string rootPath)
         {
-            ICollection<MyDirectory> dirs = null;
-            FindAddableOrRemovableDirectories(dir, ref dirs);
-            ProcessFolders(dirs);
-        }
-
-        private static void ProcessFolders(ICollection<MyDirectory> dirs)
-        {
-            try
+            if(sourceDir.CatalogItemAction == CatalogItemAction.Create)
             {
-                foreach (var item in dirs)
+                var path = Path.Combine(rootPath, sourceDir.RelativePath);
+                try
                 {
-                    switch(item.CatalogItemAction)
-                    {
-                        case CatalogItemAction.Add:
-                            Directory.CreateDirectory(item.Info.FullName);
-                            break;
-                        case CatalogItemAction.Delete:
-                            Directory.Delete(item.Info.FullName);
-                            break;
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                _logger.Error(e, "The process failed: {0}", e.Message);
-            }
-        }
-
-     
-
-        public static void AddDirectories(MyDirectory sourceDir, string rootPath)
-        {
-            try
-            {
-                if(sourceDir.CatalogItemAction == CatalogItemAction.Add)
-                {
-                    var path = Path.Combine(rootPath, sourceDir.RelativePath);
                     Directory.CreateDirectory(path);
+                    _logger.Info("The directory {name} was created", path);
                 }
-                var subDirs = sourceDir.Subdirectories;
-                if (subDirs != null)
+                catch (Exception e)
                 {
-                    foreach (var item in subDirs)
-                    {
-                        AddDirectories(item, rootPath);
-                    }
+                    _logger.Error(e, "The process of creating directory {name} was failed: {message}", sourceDir.RelativePath, e.Message);
                 }
             }
-            catch (Exception e)
-            {
-                _logger.Error(e, "The process failed: {0}", e.Message);
-            }
-        }
+            PropagateActionToSubdirectories(sourceDir, subDir => CreateDirectories(subDir, rootPath));
+        }    
 
         public static void DeleteDirectories(MyDirectory sourceDir)
         {
-            try
+            if(sourceDir.CatalogItemAction == CatalogItemAction.Delete)
             {
-                if(sourceDir.CatalogItemAction == CatalogItemAction.Delete)
+                try
                 {
                     sourceDir.Info.Delete(true);
+                    _logger.Info("The directory {name} was deleted", sourceDir.RelativePath);
                 }
-                else
+                catch (Exception e)
                 {
-                    var subDirs = sourceDir.Subdirectories;
-                    if (subDirs != null)
-                    {
-                        foreach (var item in subDirs)
-                        {
-                            DeleteDirectories(item);
-                        }
-                    }
+                    _logger.Error(e, "The process of deleting directory {name} was failed: {message}", sourceDir.RelativePath, e.Message);
                 }
             }
-            catch (Exception e)
+            else
             {
-                _logger.Error(e, "The process failed: {0}", e.Message);
+                PropagateActionToSubdirectories(sourceDir, DeleteDirectories);
             }
-        } 
+        }
 
-        private static void FindAddableOrRemovableDirectories(MyDirectory dir, ref ICollection<MyDirectory> foundDirs)
+        private static void PropagateActionToSubdirectories(MyDirectory directory, Action<MyDirectory> actionForSubDir)
         {
-            var subDirs = dir.Subdirectories;
-            if(subDirs != null)
+            var subDirs = directory.Subdirectories;
+            if (subDirs != null)
             {
-                if(dir.CatalogItemAction == CatalogItemAction.Add
-                            || dir.CatalogItemAction == CatalogItemAction.Delete)
+                foreach (var subDir in subDirs)
                 {
-                    foundDirs.Add(dir);
-                }
-                foreach (var item in subDirs)
-                {
-                    FindAddableOrRemovableDirectories(item, ref foundDirs);
+                    actionForSubDir(subDir);
                 }
             }
         }
+
+        // internal static void ManageDirectories(MyDirectory dir)
+        // {
+        //     ICollection<MyDirectory> dirs = null;
+        //     FindAddableOrRemovableDirectories(dir, ref dirs);
+        //     ProcessFolders(dirs);
+        // }
+
+        // private static void ProcessFolders(ICollection<MyDirectory> dirs)
+        // {
+        //     try
+        //     {
+        //         foreach (var item in dirs)
+        //         {
+        //             switch(item.CatalogItemAction)
+        //             {
+        //                 case CatalogItemAction.Add:
+        //                     Directory.CreateDirectory(item.Info.FullName);
+        //                     break;
+        //                 case CatalogItemAction.Delete:
+        //                     Directory.Delete(item.Info.FullName);
+        //                     break;
+        //             }
+        //         }
+        //     }
+        //     catch (Exception e)
+        //     {
+        //         _logger.Error(e, "The process failed: {0}", e.Message);
+        //     }
+        // }
+
+        private static void ProcessFile(IEnumerable<MyFile> files, string rootPath, CatalogItemAction action, Action<string, string> actionFunc)
+        {
+            var processableFiles = files.Where(i => i.CatalogItemAction == action);
+            
+            foreach (var item in processableFiles)
+            {
+                try
+                {
+                    var sourceFileName = item.Info.FullName;
+                    var destFileName = Path.Combine(rootPath, item.RelativePath);
+                    actionFunc(sourceFileName, destFileName);
+                    _logger.Info("The file {file} was {operation}ed ", item.RelativePath, action);
+                }
+                catch(Exception e)
+                {
+                    _logger.Error(e, "Couldn't {action} the file {name}", action, item.RelativePath);
+                }
+            }
+        }
+
+
+
+        // private static void FindAddableOrRemovableDirectories(MyDirectory dir, ref ICollection<MyDirectory> foundDirs)
+        // {
+        //     var subDirs = dir.Subdirectories;
+        //     if(subDirs != null)
+        //     {
+        //         if(dir.CatalogItemAction == CatalogItemAction.Create
+        //                     || dir.CatalogItemAction == CatalogItemAction.Delete)
+        //         {
+        //             foundDirs.Add(dir);
+        //         }
+        //         foreach (var item in subDirs)
+        //         {
+        //             FindAddableOrRemovableDirectories(item, ref foundDirs);
+        //         }
+        //     }
+        // }
     }
 }
